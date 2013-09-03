f751af61d5347da25435c2517d8c9fe2425c97a9
wllwen007 <wllwen007@gmail.com>
2013-09-03 10:13:45 +0000

    Update to allow run_ndppp to specify the msin and msout parameters and a parset file (without msin  and msout parameters) to run on different subbands.

--- mpip/tasks/run_ndppp.py
+++ mpip/tasks/run_ndppp.py
@@ -10,7 +10,7 @@ class Run_ndppp(Task):
     __name__ = 'run_ndppp'
     __doc__ = 'doc for run_ndppp'
     banner = 'RUN_ndppp: run many istances of ndppp'
-    arg_list = ['ndppp_parset','sb','group','node','obsdir','msname']
+    arg_list = ['ndppp_parset','sb','group','node','obsdir','msin','msout']
 
     def __init__(self, session):
         Task.__init__(self, session, self.__name__)
@@ -24,7 +24,15 @@ class Run_ndppp(Task):
             wdir = self.session.opts.get_opt('obsdir')+'/'+self.session._obs_name+'/group$SB/'
         else:
             wdir = self.session.opts.get_opt('obsdir')+'/'+self.session._obs_name+'/SB$SB/'
-        datafile = self.session.opts.get_opt('msname')
-        command = 'NDPPP ' + self.session.opts.get_opt('ndppp_parset') + ' >> run_ndppp.log'
+        # changes to add different msin and msout '
+        # the parset must not contain the msin and msout parameters, these are 
+        #  added to the parset here so we can run the same parset on many 
+        #  different subbands
+        msin = self.session.opts.get_opt('msin')
+        msout = self.session.opts.get_opt('msout')
+        parset = self.session.opts.get_opt('ndppp_parset')
+        # command = 'NDPPP ' + parset + ' >> run_ndppp.log
+        command = 'echo msin='+msin+' > temp.parset ;' + 'echo msout='+msout+' >> temp.parset ;' + 'cat '+parset+'>> temp.parset  && NDPPP temp.parset >> run_ndppp.log && rm temp.parset'
         return self.session.run_dist_com_all('run_ndppp', wdir, command, \
               self.session.opts.get_opt('sb'), self.session.opts.get_opt('node'), self.session.opts.get_opt('group'))
+
